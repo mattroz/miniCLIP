62e249be36a7161247d09d4d6c8fd9d96e67a162
mattroz <matveyrozanov@gmail.com>
2024-01-06 15:32:27 +0000

    added CLIP builder

---
+++ src/model/__init__.py
@@ -0,0 +1,31 @@
+from .clip import CLIP
+from .text_encoder import TextEncoderV1
+from .vision_encoder import VisionEncoderV1
+
+
+def make_CLIP(
+        vocab_size, 
+        vision_encoder_name="resnet50", 
+        embed_dim=512,
+        sequence_length=16,
+        text_encoder_num_layers=8,
+        text_encoder_num_heads=8,
+        temperature=0.07
+):
+    vision_encoder = VisionEncoderV1(model_name=vision_encoder_name, 
+                                     out_features=embed_dim)
+    
+    text_encoder = TextEncoderV1(vocab_size=vocab_size, 
+                                 d_model=embed_dim, 
+                                 n_layers=text_encoder_num_layers, 
+                                 n_heads=text_encoder_num_heads, 
+                                 sequence_length=sequence_length)
+
+    clip = CLIP(vision_encoder, 
+                text_encoder, 
+                text_embed_dim=embed_dim, 
+                vision_embed_dim=embed_dim, 
+                embed_dim=embed_dim, 
+                temperature=temperature)
+
+    return clip
